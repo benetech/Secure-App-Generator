c64fcd384eb6e023f25dfc882e03a41072f4f83f
Charles LaPierre <charlesl@benetech.org>
2015-05-15 12:09:30 -0700

    MARTUSDEV-1440 Create the SAG Initial Project MARTUSDEV-1441 Create & Add SAG new Project to a Benetech Git repository

---
+++ SecureAppGenerator/src/main/java/SAG/SecureAppGeneratorApplication.java
@@ -0,0 +1,12 @@
+package SAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAppGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureAppGeneratorApplication.class, args);
+    }
+}
