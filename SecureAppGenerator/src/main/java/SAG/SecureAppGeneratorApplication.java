18dc1d14e014fe9d314f0e5f5bb4521d2c822dbb
Charles LaPierre <charlesl@benetech.org>
2015-05-18 09:24:41 -0700

    adjust brace placement

--- SecureAppGenerator/src/main/java/SAG/SecureAppGeneratorApplication.java
+++ SecureAppGenerator/src/main/java/SAG/SecureAppGeneratorApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecureAppGeneratorApplication {
+public class SecureAppGeneratorApplication 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         SpringApplication.run(SecureAppGeneratorApplication.class, args);
     }
 }
