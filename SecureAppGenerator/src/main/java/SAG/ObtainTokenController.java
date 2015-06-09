64b7d18b9681793a97bbb1ddd5f2bff97194b264
Charles LaPierre <charlesl@benetech.org>
2015-06-09 07:11:22 -0700

    Use Martus-api jar instead of the quick copy/paste hack previously done.

--- SecureAppGenerator/src/main/java/SAG/ObtainTokenController.java
+++ SecureAppGenerator/src/main/java/SAG/ObtainTokenController.java
@@ -31,14 +31,14 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.martus.common.MartusAccountAccessToken;
+import org.martus.common.MartusAccountAccessToken.TokenInvalidException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import SAG.MartusAccountAccessToken.TokenInvalidException;
 
 @Controller
 public class ObtainTokenController extends WebMvcConfigurerAdapter
