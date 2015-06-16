98716465f5f289a0e2faf0ae4951773885306004
Charles LaPierre <charlesl@benetech.org>
2015-06-16 10:21:24 -0700

    Get real PublicCode for account from Martus Server.

--- SecureAppGenerator/src/main/java/SAG/ObtainTokenController.java
+++ SecureAppGenerator/src/main/java/SAG/ObtainTokenController.java
@@ -32,14 +32,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Vector;
 
 import javax.servlet.http.HttpSession;
 
+import org.martus.clientside.ClientSideNetworkGateway;
 import org.martus.common.DammCheckDigitAlgorithm.CheckDigitInvalidException;
+import org.martus.common.Exceptions.ServerCallFailedException;
+import org.martus.common.Exceptions.ServerNotAvailableException;
+import org.martus.common.Exceptions.ServerNotCompatibleException;
 import org.martus.common.MartusAccountAccessToken;
 import org.martus.common.MartusAccountAccessToken.TokenInvalidException;
+import org.martus.common.MartusAccountAccessToken.TokenNotFoundException;
 import org.martus.common.crypto.MartusCrypto;
 import org.martus.common.crypto.MartusCrypto.CreateDigestException;
+import org.martus.common.crypto.MartusSecurity;
+import org.martus.common.network.ClientSideNetworkInterface;
+import org.martus.common.network.NetworkInterfaceConstants;
+import org.martus.common.network.NetworkResponse;
+import org.martus.common.network.PassThroughTransportWrapper;
+import org.martus.common.network.TransportWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -50,12 +62,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Controller
 public class ObtainTokenController extends WebMvcConfigurerAdapter
 {
+	private static final String SL1_DEVELOPMENT_NAME = "SL1 Development";
+	private static final String SL1_IE_NAME = "SL1 IE";
 	private static final char DASH_CHAR = '-';
 	private static final String APK_EXTENSION = ".apk";
 	private static final char UNDERSCORE_CHAR = '_';
 	private static final char SPACE_CHAR = ' ';
 	public static final String DESKTOP_PUBLIC_KEY = "MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAhToihLM3R540behcXLDKSHXjhiRsL3oQdyWeUfphpYXIQydfPny40zoFoznC3YwM8Jykf9ToQstO1k74SYwRuIEEdtews6ETV7U8sDz5IZnjOOg4xtBIEAAtFkO23oG429i1scOFI9L9p8xkhVePeZZ4CNHBXztYDcKVcqn+cEn8aTqBZ0sdcOUkZlMlfR628GhhCekS1lm0t6CSdRWKyqvGRJ6RbROep16ATZdriaJiPyVKMy1y/mAIoz/rRIOCUphnDTQjlKox6sQ6EaWykLCn0oY8KAR87Rar0OY09fuEn2KkP3gdhzVmmITFrGTFi37kzGDfCjJ8tn86G2D8KvAyiDdi8OlgSpDqI/G1MzPB04vrQa/HRcAwT1W7qmd0pKwu+GmQ+o9j1oiOpvFrK48TedZbI9fUWqXfuNSvS1pukfJ+svAPLlo+rBCT6F0hoGYgSFWUzRiiZuKZR4au51tqxhU4Qk7PDcFe/kx3TrtJKYLkLM1jQgT0ERdtlPZ9AgMBAAE=";
-
+	private static final String SAG_KEYPAIR_LOCATION = "/Users/charlesl/SAG/sagKeyPair.dat"; 
+	private static final String SAG_KEYPAIR_PASSWORD = "$$SaGPassword";
 	@RequestMapping(value=WebPage.OBTAIN_CLIENT_TOKEN, method=RequestMethod.GET)
     public String directError(HttpSession session, Model model) 
     {
@@ -77,12 +92,20 @@ public class ObtainTokenController extends WebMvcConfigurerAdapter
     {
 		if(isValidToken(appConfig)) 
 		{
-			if(getClientPublicKeyFromToken(session, appConfig))
+			try
 			{
-				updateServerConfiguration(session);
-				updateApkVersionInfoAndName(session);
- 				model.addAttribute(SessionAttributes.APP_CONFIG, appConfig);
-				return WebPage.SUMMARY;
+				if(getClientPublicKeyFromToken(session, appConfig))
+				{
+					updateServerConfiguration(session);
+					updateApkVersionInfoAndName(session);
+					model.addAttribute(SessionAttributes.APP_CONFIG, appConfig);
+					return WebPage.SUMMARY;
+				}
+			}
+			catch (Exception e)
+			{
+				appConfig.setClientTokenError("Error: Unable to retrieve token from server.");
+				e.printStackTrace();
 			}
 		}
 		model.addAttribute(SessionAttributes.APP_CONFIG, appConfig);
@@ -158,23 +181,53 @@ public class ObtainTokenController extends WebMvcConfigurerAdapter
 	{
         AppConfiguration config = (AppConfiguration)session.getAttribute(SessionAttributes.APP_CONFIG);
 		if(ServerConstants.usingRealServer())
-			config.setServerName("SL1 IE");
+			config.setServerName(SL1_IE_NAME);
 		else
-			config.setServerName("SL1 Development");
+			config.setServerName(SL1_DEVELOPMENT_NAME);
 		config.setServerIP(ServerConstants.getCurrentServerIp());
 		config.setServerPublicCode(ServerConstants.getCurrentSeverKey());
 		session.setAttribute(SessionAttributes.APP_CONFIG, config);
 	}
 
+	private ClientSideNetworkInterface createXmlRpcNetworkInterfaceHandler()
+	{
+		TransportWrapper transport = new PassThroughTransportWrapper();
+		String ourServer = ServerConstants.getCurrentServerIp();
+		String ourServerPublicKey = ServerConstants.getCurrentSeverKey();
+		return ClientSideNetworkGateway.buildNetworkInterface(ourServer, ourServerPublicKey, transport);
+	}
+
 	// TODO add unit tests
-	private boolean getClientPublicKeyFromToken(HttpSession session, AppConfiguration appConfig)
+	private boolean getClientPublicKeyFromToken(HttpSession session, AppConfiguration appConfig) throws Exception
 	{
 		//TODO get real key from Token server.
         AppConfiguration config = (AppConfiguration)session.getAttribute(SessionAttributes.APP_CONFIG);
- 		config.setClientPublicKey(DESKTOP_PUBLIC_KEY);
  		try
 		{
-			config.setClientPublicCode(MartusCrypto.computeFormattedPublicCode40(DESKTOP_PUBLIC_KEY));
+ 			//TODO do this once not ever time this page gets displayed
+ 			MartusSecurity security = new MartusSecurity();
+ 			ClientSideNetworkGateway gateway = new ClientSideNetworkGateway(createXmlRpcNetworkInterfaceHandler());
+			File keyPair = new File(SAG_KEYPAIR_LOCATION);
+			security.readKeyPair(keyPair, SAG_KEYPAIR_PASSWORD.toCharArray());
+ 			
+ 			String tokenString = appConfig.getClientToken();
+			MartusAccountAccessToken accessToken = new MartusAccountAccessToken(tokenString);
+ 			NetworkResponse response = gateway.getMartusAccountIdFromAccessToken(security, accessToken);
+ 			if(response.getResultCode().equals(NetworkInterfaceConstants.NO_TOKEN_AVAILABLE))
+ 				throw new TokenNotFoundException();
+ 			if(response.getResultCode().equals(NetworkInterfaceConstants.SERVER_NOT_COMPATIBLE))
+ 				throw new ServerNotCompatibleException();
+ 			if(response.getResultCode().equals(NetworkInterfaceConstants.SERVER_ERROR))
+ 				throw new ServerCallFailedException();
+ 			if(!response.getResultCode().equals(NetworkInterfaceConstants.OK))
+ 				throw new ServerNotAvailableException();
+ 						
+ 			Vector singleAccountId = response.getResultVector();
+ 			if(singleAccountId.size() != 1)
+ 				throw new TokenNotFoundException();
+ 			String AccountId = (String)singleAccountId.get(0);
+	 		config.setClientPublicKey(AccountId);
+			config.setClientPublicCode(MartusCrypto.computeFormattedPublicCode40(AccountId));
 		}
 		catch (CreateDigestException | CheckDigitInvalidException e)
 		{
