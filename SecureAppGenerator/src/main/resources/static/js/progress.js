1ed8eacfe5dacc09a490c00911ac4d390ae5272a
Charles LaPierre <charlesl@benetech.org>
2015-06-18 16:30:41 -0700

    Work on New Progress Meter for building app

--- SecureAppGenerator/src/main/resources/static/js/progress.js
+++ SecureAppGenerator/src/main/resources/static/js/progress.js
@@ -3,9 +3,10 @@ function showAlert()
 	alert('Hello from JavaScript!');
 }
 
-function showBuildingProgressBar()
+function initalize()
 {
-//	this.document.getElementById("progressBar").style.display = 'block';
+	this.document.getElementById('cellphone_center_title').innerHTML = "";
+	this.document.getElementById('cellphone_center_data').innerHTML = "";
 	this.document.getElementById("next").disabled = true;
 	this.document.getElementById("next").style.visibility = "hidden";
 	this.document.getElementById("previous").disabled = true;
@@ -37,3 +38,65 @@ function startNewTimer() {
     		display = this.document.getElementById("progressArea");
     startTimer(twoMinutes, display);
 };
+
+
+var myProgressBar = null
+var timerId = null
+
+function loadProgressBar() {
+	myProgressBar = new ProgressBar("my_progress_bar_1",{
+		borderRadius: 10,
+		width: 300,
+		height: 20,
+		maxValue: 100,
+		labelText: "Loaded in {value,0} %",
+		orientation: ProgressBar.Orientation.Horizontal,
+		direction: ProgressBar.Direction.LeftToRight,
+		animationStyle: ProgressBar.AnimationStyle.LeftToRight1,
+		animationSpeed: 1.5,
+		imageUrl: 'images/v_fg12.png',
+		backgroundUrl: 'images/h_bg2.png',
+		markerUrl: 'images/marker2.png'
+	});
+	
+	timerId = window.setInterval(function() {
+		if (myProgressBar.value >= myProgressBar.maxValue)
+			myProgressBar.setValue(0);
+		else
+			myProgressBar.setValue(myProgressBar.value+1);
+		
+	},
+	100);
+}
+
+function showProgress(buildText)
+{
+	initalize();
+	var myCanvas = this.document.getElementById('ProgressArea');
+	
+	var percentComplete = 0;
+	
+	var circle = new ProgressCircle({
+	    canvas: myCanvas, 
+	    minRadius: 100, // Inner radius of the innermost circle
+	    arcWidth: 20, // Width of each circle
+	    centerX: 270, // X coordinate of the circle center
+	    centerY: 140, // Y coordinate of the circle center
+	    infoLineLength: 100, // Length of the info line
+        horizLineLength: -30, // Length of the horizontal info line
+        infoLineBaseAngle: 130.4,
+        });
+	
+	
+	circle.addEntry({
+	    fillColor: '#6ECDE4',
+	    progressListener: function() {
+	    	percentComplete = percentComplete + .01;
+	    	if(percentComplete > 1)
+	    		return 1;
+	    	return percentComplete;},
+	    	infoListener: function() {return buildText;},// (Optional)	    	
+	 }); 		
+	circle.start(1200); 	
+}
+
