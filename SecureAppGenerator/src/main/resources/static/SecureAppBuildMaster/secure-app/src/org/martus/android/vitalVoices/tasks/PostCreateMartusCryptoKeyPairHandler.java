64cb863e66ced8e2a54866da8711d7b59baecda7
Charles LaPierre <charlesl@benetech.org>
2015-07-22 06:22:44 -0700

    not going to check in this all into SAG, but add the SA during the gradle build instead.

--- SecureAppGenerator/src/main/resources/static/SecureAppBuildMaster/secure-app/src/org/martus/android/vitalVoices/tasks/PostCreateMartusCryptoKeyPairHandler.java
+++
@@ -1,9 +0,0 @@
-package org.martus.android.vitalVoices.tasks;
-
-/**
- * Created by animal@martus.org on 10/3/14.
- */
-public interface PostCreateMartusCryptoKeyPairHandler {
-
-    public void postCreateMartusCryptoKeyPairExecute();
-}
--- SecureAppGenerator/src/main/resources/static/SecureAppBuildMaster/secure-app/src/org/martus/android/vitalVoices/tasks/ProgressUpdater.java
+++
@@ -1,9 +0,0 @@
-package org.martus.android.vitalVoices.tasks;
-
-/**
- * @author roms
- *         Date: 1/11/13
- */
-public interface ProgressUpdater {
-    public void showProgress(int value);
-}
