3db2348a47fe4282fc88fea3cf6dcb0a8d6b679e
Charles LaPierre <charlesl@benetech.org>
2015-07-24 11:43:09 -0700

    SAG-145 Validate the XFORM a user uploads to make sure it is valid xforms data

--- SecureAppGenerator/src/main/java/org/benetech/secureapp/generator/ObtainXFormController.java
+++ SecureAppGenerator/src/main/java/org/benetech/secureapp/generator/ObtainXFormController.java
@@ -25,6 +25,7 @@ Boston, MA 02111-1307, USA.
 
 package org.benetech.secureapp.generator;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -34,13 +35,23 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.commons.io.FileUtils;
-import org.javarosa.core.util.JavaRosaCoreModule;
-import org.javarosa.model.xform.XFormsModule;
+import org.javarosa.core.model.FormDef;
+import org.javarosa.core.model.FormIndex;
+import org.javarosa.core.model.GroupDef;
+import org.javarosa.core.model.IFormElement;
+import org.javarosa.core.model.QuestionDef;
+import org.javarosa.form.api.FormEntryController;
+import org.javarosa.form.api.FormEntryModel;
+import org.javarosa.form.api.FormEntryPrompt;
+import org.javarosa.xform.util.XFormUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -49,6 +60,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 
+ *  DataTypes and ControlTypes
+ *  
+ * 	DATATYPE_UNSUPPORTED = -1;
+ *  DATATYPE_NULL = 0;  			For nodes that have no data, or data type otherwise unknown
+ *  DATATYPE_TEXT = 1;    		Text question type. 
+ *  DATATYPE_INTEGER = 2;  		Numeric question type. These are numbers without decimal points
+ *  DATATYPE_DECIMAL = 3;   		Decimal question type. These are numbers with decimals
+ *  DATATYPE_DATE = 4;    		Date question type. This has only date component without time.
+ *  DATATYPE_TIME = 5;    		Time question type. This has only time element without date
+ *  DATATYPE_DATE_TIME = 6;		Date and Time question type. This has both the date and time components
+ *  DATATYPE_CHOICE = 7;			This is a question with alist of options where not more than one option can be selected at a time.
+ *  DATATYPE_CHOICE_LIST = 8;	This is a question with alist of options where more than one option can be selected at a time.
+ *  DATATYPE_BOOLEAN = 9;    	Question with true and false answers.
+ *  DATATYPE_GEOPOINT = 10; 		Question with location answer.
+ *  DATATYPE_BARCODE = 11; 		Question with barcode string answer.
+ *  DATATYPE_BINARY = 12; 		Question with external binary answer.
+ *  DATATYPE_LONG = 13; 			Question with external binary answer. 
+ *  DATATYPE_GEOSHAPE = 14; 		Question with GeoShape answer.
+ *  DATATYPE_GEOTRACE = 15; 		Question with GeoTrace answer.
+ *
+ *  CONTROL_UNTYPED = -1;
+ *  CONTROL_INPUT = 1;
+ *  CONTROL_SELECT_ONE = 2;
+ *  CONTROL_SELECT_MULTI = 3;
+ *  CONTROL_TEXTAREA = 4;
+ *  CONTROL_SECRET = 5;
+ *  CONTROL_RANGE = 6;
+ *  CONTROL_UPLOAD = 7;
+ *  CONTROL_SUBMIT = 8;
+ *  CONTROL_TRIGGER = 9;
+ *  CONTROL_IMAGE_CHOOSE = 10;
+ *  CONTROL_LABEL = 11;
+ *  CONTROL_AUDIO_CAPTURE = 12;
+ *  CONTROL_VIDEO_CAPTURE = 13;
+ */
 
 @Controller
 public class ObtainXFormController extends WebMvcConfigurerAdapter
@@ -121,7 +170,15 @@ public class ObtainXFormController extends WebMvcConfigurerAdapter
             }
         }
 		
-		isValidXForm(new File(xFormLocation));
+		try
+		{
+			isValidXForm();
+		}
+		catch (Exception e)
+		{
+    			Logger.logException(session, e);
+       		return returnErrorMessage(model, appConfig, "Error: xForm invalid: " + e.getLocalizedMessage()); 
+		}
 		
 		AppConfiguration config = (AppConfiguration)session.getAttribute(SessionAttributes.APP_CONFIG);
 		config.setAppXFormLocation(xFormLocation);
@@ -131,11 +188,180 @@ public class ObtainXFormController extends WebMvcConfigurerAdapter
 		return WebPage.OBTAIN_CLIENT_TOKEN;
     }
 
-	private void isValidXForm(File xFormFile)
+	private void isValidXForm() throws Exception
 	{
+		File xformFile = new File(XML_FILE_LOCATION);
 		
-		//	void xformsModule = new XFormsModule().registerModule();
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		dBuilder.parse(xformFile);
+
+		byte[] xformRawData = Files.readAllBytes(xformFile.toPath());
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(xformRawData);
+        FormDef formDef = XFormUtils.getFormFromInputStream(byteArrayInputStream);
+        FormEntryModel formEntryModel = new FormEntryModel(formDef);
+        StringBuilder fieldErrors = new StringBuilder();
+        inspectFields(formEntryModel, formDef, fieldErrors);
+        if(fieldErrors.length() != 0)
+    			throw new Exception("Unsupported XForm Field(s): " +fieldErrors.toString());
 	}
+	
+	private void inspectFields(FormEntryModel model, FormDef formDef, StringBuilder fieldErrors)
+    {
+         List<IFormElement> children = formDef.getChildren();
+         recursivelyInspectFields(children, fieldErrors);
+         checkModel(model, fieldErrors);
+    }
+
+    private void checkModel(FormEntryModel model, StringBuilder fieldErrors)
+	{
+		FormIndex index = model.getFormIndex();
+		while(!index.isEndOfFormIndex())
+		{
+			int event = model.getEvent(index);
+			if(event != FormEntryController.EVENT_QUESTION)
+			{
+				index = model.incrementIndex(index);
+				continue;
+			}
+			FormEntryPrompt prompt = model.getQuestionPrompt(index);
+			final int dataType = prompt.getDataType();
+			switch(dataType)
+			{
+				case org.javarosa.core.model.Constants.DATATYPE_TEXT:
+ 				case org.javarosa.core.model.Constants.DATATYPE_DATE:
+ 				case org.javarosa.core.model.Constants.DATATYPE_CHOICE:
+ 				case org.javarosa.core.model.Constants.DATATYPE_INTEGER:
+ 				case org.javarosa.core.model.Constants.DATATYPE_DECIMAL:
+				case org.javarosa.core.model.Constants.DATATYPE_LONG:
+ 				case org.javarosa.core.model.Constants.DATATYPE_TIME:
+ 				case org.javarosa.core.model.Constants.DATATYPE_BOOLEAN:
+ 					break;
+
+ 				//Unsupported DataType's below
+ 				case org.javarosa.core.model.Constants.DATATYPE_BARCODE:
+ 					addFieldTypeNotSupported("BARCODE", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_BINARY:
+ 					addFieldTypeNotSupported("BINARY", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_CHOICE_LIST:
+ 					addFieldTypeNotSupported("CHOICE_LIST", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_DATE_TIME:
+ 					addFieldTypeNotSupported("DATE_TIME", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_GEOPOINT:
+ 					addFieldTypeNotSupported("GEOPOINT", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_GEOSHAPE:
+ 					addFieldTypeNotSupported("GEOSHAPE", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_GEOTRACE:
+ 					addFieldTypeNotSupported("GEOTRACE", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_NULL:
+ 					addFieldTypeNotSupported("NULL", fieldErrors, prompt);
+ 					break;
+ 				case org.javarosa.core.model.Constants.DATATYPE_UNSUPPORTED:
+ 					addFieldTypeNotSupported("UNSUPPORTED", fieldErrors, prompt);
+ 					break;
+ 				default:
+ 					addFieldTypeNotSupported("UNKNOWN", fieldErrors, prompt);
+ 					break;
+            	
+			}
+			index = model.incrementIndex(index);
+		}
+	}
+
+	private void recursivelyInspectFields(List<IFormElement> children, StringBuilder fieldErrors)
+    {
+        for (IFormElement child : children)
+        {
+            if (child instanceof GroupDef)
+            {
+                GroupDef groupDef = (GroupDef) child;
+                List<IFormElement> groupChildren = groupDef.getChildren();
+                recursivelyInspectFields(groupChildren, fieldErrors);
+            }
+ 
+            if (child instanceof QuestionDef)
+            {
+                QuestionDef questionDef = (QuestionDef) child;
+                final int controlType = questionDef.getControlType();
+ 	            switch(controlType)
+	            {
+	            case org.javarosa.core.model.Constants.CONTROL_INPUT:
+	            case org.javarosa.core.model.Constants.CONTROL_SELECT_ONE:
+	            case org.javarosa.core.model.Constants.CONTROL_LABEL:
+	            case org.javarosa.core.model.Constants.CONTROL_TEXTAREA:
+ 	            case org.javarosa.core.model.Constants.CONTROL_TRIGGER:
+ 	            		break;
+
+ 	            	//Unsupported Control types below
+	            case org.javarosa.core.model.Constants.CONTROL_SELECT_MULTI:
+        				addFieldControlNotSupported("SELECT_MULTI", fieldErrors, questionDef);
+        				break;
+	            case org.javarosa.core.model.Constants.CONTROL_AUDIO_CAPTURE:
+	            		addFieldControlNotSupported("AUDIO_CAPTURE", fieldErrors, questionDef);
+	            		break;
+	            case org.javarosa.core.model.Constants.CONTROL_IMAGE_CHOOSE:
+        				addFieldControlNotSupported("IMAGE_CHOOSE", fieldErrors, questionDef);
+	            		break;
+	            case org.javarosa.core.model.Constants.CONTROL_RANGE:
+        				addFieldControlNotSupported("RANGE", fieldErrors, questionDef);
+            			break;
+	            case org.javarosa.core.model.Constants.CONTROL_SECRET:
+        				addFieldControlNotSupported("SECRET", fieldErrors, questionDef);
+            			break;
+	            case org.javarosa.core.model.Constants.CONTROL_SUBMIT:
+        				addFieldControlNotSupported("SUBMIT", fieldErrors, questionDef);
+            			break;
+	            case org.javarosa.core.model.Constants.CONTROL_UNTYPED:
+        				addFieldControlNotSupported("UNTYPED", fieldErrors, questionDef);
+            			break;
+	            case org.javarosa.core.model.Constants.CONTROL_UPLOAD:
+        				addFieldControlNotSupported("UPLOAD", fieldErrors, questionDef);
+            			break;
+	            case org.javarosa.core.model.Constants.CONTROL_VIDEO_CAPTURE:
+        				addFieldControlNotSupported("VIDEO_CAPTURE", fieldErrors, questionDef);
+            			break;
+	            	default:
+            			addFieldControlNotSupported("UNKNOWN", fieldErrors, questionDef);
+	            		break;
+	            }
+           }
+        }
+     }
+
+	private void addFieldTypeNotSupported(String fieldType, StringBuilder fieldErrors, FormEntryPrompt prompt)
+	{
+		StringBuilder errorMsg = new StringBuilder(fieldType);
+		errorMsg.append(" : \"");
+		errorMsg.append(prompt.getQuestion().getTextID());
+		errorMsg.append("\"");
+		addErrorToListOfErrors(fieldErrors, errorMsg);
+	}
+
+	private void addFieldControlNotSupported(String fieldName, StringBuilder fieldErrors, QuestionDef questionDef)
+	{
+		StringBuilder errorMsg = new StringBuilder(fieldName);
+		errorMsg.append(" : id=");
+		errorMsg.append(questionDef.getID());
+		errorMsg.append(" : label=");
+		errorMsg.append(questionDef.getLabelInnerText());
+		addErrorToListOfErrors(fieldErrors, errorMsg);
+	}
+
+	private void addErrorToListOfErrors(StringBuilder fieldErrors, StringBuilder errorMsg)
+	{
+		if(fieldErrors.length() != 0)
+			fieldErrors.append(", ");
+		fieldErrors.append("[");
+		fieldErrors.append(errorMsg);
+		fieldErrors.append("]");
+	}	
 
 	public String returnErrorMessage(Model model, AppConfiguration appConfig, String errorMsg)
 	{
