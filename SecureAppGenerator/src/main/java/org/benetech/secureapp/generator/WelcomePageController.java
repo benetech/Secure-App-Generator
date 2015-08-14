9ca752670b6e74322d9ab5d8ef3bcb01d71bbbf8
Charles LaPierre <charlesl@benetech.org>
2015-08-14 16:06:42 -0700

    adding localization properties and utilizing thymeleaf's mechognism to access property files.

--- SecureAppGenerator/src/main/java/org/benetech/secureapp/generator/WelcomePageController.java
+++ SecureAppGenerator/src/main/java/org/benetech/secureapp/generator/WelcomePageController.java
@@ -32,9 +32,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Controller
-public class WelcomePageController
+public class WelcomePageController extends WebMvcConfigurerAdapter
 {
 	@RequestMapping({"/"})
 	String index(HttpSession session) throws Exception 
