8d2513a7da0c126477cf53513c1599520f036ea1
Charles LaPierre <charlesl@benetech.org>
2015-08-19 07:31:12 -0700

    cleaned up unused functions from Java Script

--- SecureAppGenerator/src/main/resources/static/js/progress.js
+++ SecureAppGenerator/src/main/resources/static/js/progress.js
@@ -1,8 +1,3 @@
-function showAlert()
-{
-	alert('Hello from JavaScript!');
-}
-
 function initalize()
 {
 	this.document.getElementById('cellphone_center_title').innerHTML = "";
@@ -11,62 +6,8 @@ function initalize()
 	this.document.getElementById("next").style.visibility = "hidden";
 	this.document.getElementById("previous").disabled = true;
 	this.document.getElementById("previous").style.visibility = "hidden";
+
 	this.document.body.style.cursor = 'wait';
-}
-
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = "Building APK: " + minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-function startNewTimer() {
-	showBuildingProgressBar();
-	
-    var twoMinutes = 60 * 2,
-    		display = this.document.getElementById("progressArea");
-    startTimer(twoMinutes, display);
-};
-
-
-var myProgressBar = null
-var timerId = null
-
-function loadProgressBar() {
-	myProgressBar = new ProgressBar("my_progress_bar_1",{
-		borderRadius: 10,
-		width: 300,
-		height: 20,
-		maxValue: 100,
-		labelText: "Loaded in {value,0} %",
-		orientation: ProgressBar.Orientation.Horizontal,
-		direction: ProgressBar.Direction.LeftToRight,
-		animationStyle: ProgressBar.AnimationStyle.LeftToRight1,
-		animationSpeed: 1.5,
-		imageUrl: 'images/v_fg12.png',
-		backgroundUrl: 'images/h_bg2.png',
-		markerUrl: 'images/marker2.png'
-	});
-	
-	timerId = window.setInterval(function() {
-		if (myProgressBar.value >= myProgressBar.maxValue)
-			myProgressBar.setValue(0);
-		else
-			myProgressBar.setValue(myProgressBar.value+1);
-		
-	},
-	100);
 }
 
 function showProgress(buildText)
