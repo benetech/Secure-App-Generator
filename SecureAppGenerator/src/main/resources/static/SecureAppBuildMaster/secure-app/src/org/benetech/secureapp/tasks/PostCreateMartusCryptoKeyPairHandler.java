8b9337b9b41c4a6efcbc9a12ca2aa2694fc9c60f
Charles LaPierre <charlesl@benetech.org>
2015-09-08 13:31:04 -0700

    missing SecureApp files in WAR file.

---
+++ SecureAppGenerator/src/main/resources/static/SecureAppBuildMaster/secure-app/src/org/benetech/secureapp/tasks/PostCreateMartusCryptoKeyPairHandler.java
@@ -0,0 +1,9 @@
+package org.benetech.secureapp.tasks;
+
+/**
+ * Created by animal@martus.org on 10/3/14.
+ */
+public interface PostCreateMartusCryptoKeyPairHandler {
+
+    public void postCreateMartusCryptoKeyPairExecute();
+}
