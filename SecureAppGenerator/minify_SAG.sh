6c04c3ce1edae031407a3f7ae395476e74af7858
Charles LaPierre <charlesl@benetech.org>
2015-11-11 11:23:03 -0800

    Minified all JS in SAG Bumped Version # to 25

---
+++ SecureAppGenerator/minify_SAG.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export MINIFY=./thirdparty/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar;
+export SRC=./JavaScript/src;
+export DEST=./JavaScript/minified;
+
+java -jar $MINIFY $SRC/progress.js -o $DEST/progress.js
+java -jar $MINIFY $SRC/ProgressCircle.js -o $DEST/ProgressCircle.js
+java -jar $MINIFY $SRC/xFormSelection.js -o $DEST/xFormSelection.js
