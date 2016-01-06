aae85b080b75cb10671f65c447617d668592bcd8
Charles LaPierre <charlesl@benetech.org>
2016-01-06 13:19:09 -0800

    SA updates from NIMA

--- SecureAppGenerator/src/main/resources/static/SecureAppBuildMaster/secure-app/src/org/benetech/secureapp/tasks/PostCreateMartusCryptoKeyPairHandler.java
+++
@@ -1,9 +0,0 @@
-package org.benetech.secureapp.tasks;
-
-/**
- * Created by animal@martus.org on 10/3/14.
- */
-public interface PostCreateMartusCryptoKeyPairHandler {
-
-    public void postCreateMartusCryptoKeyPairExecute();
-}
