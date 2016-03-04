c20bbe99fc495de3de7733a437f6b3f44a1265fb
Charles LaPierre <charlesl@benetech.org>
2016-03-04 14:59:54 -0800

    Try just not pinging the AWS server.

--- SecureAppGenerator/src/main/java/org/benetech/secureapp/generator/SmokeTestPageController.java
+++ SecureAppGenerator/src/main/java/org/benetech/secureapp/generator/SmokeTestPageController.java
@@ -68,8 +68,8 @@ public class SmokeTestPageController extends WebMvcConfigurerAdapter
 		AppConfiguration config = new AppConfiguration();
 		smokeResults.append(KEY_SAG_VERSION, config.getApkVersionNumberFull());
 		setMartusServerUsed(config, smokeResults);
-		//testMartusTokenServer(session, config, smokeResults);
-		testAmazonS3Server(session, smokeResults);
+		testMartusTokenServer(session, config, smokeResults);
+		//testAmazonS3Server(session, smokeResults);
 		testFreeDiskSpace(session, smokeResults);
 		File originalBuildDirectory = new File(SecureAppGeneratorApplication.getOriginalBuildDirectory());
 		testFilesExist(session, MASTER_BUILD_FILES, originalBuildDirectory,  smokeResults);
